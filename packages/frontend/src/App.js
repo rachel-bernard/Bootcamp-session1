417c95cfc171023b045067374e702d126a49706e
rachel-bernard <163907397+rachel-bernard@users.noreply.github.com>
2026-03-18 16:25:06 +0000

    fix it to say hello world instead i guess

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -58,7 +58,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Rachel's cool app</h1>
+        <h1>Hello World</h1>
         <p>Connected to in-memory database</p>
       </header>
       
--- packages/frontend/src/__tests__/App.test.js
+++ packages/frontend/src/__tests__/App.test.js
@@ -50,7 +50,7 @@ describe('App Component', () => {
     await act(async () => {
       render(<App />);
     });
-    expect(screen.getByText("Rachel's cool app")).toBeInTheDocument();
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
     expect(screen.getByText('Connected to in-memory database')).toBeInTheDocument();
   });
 
